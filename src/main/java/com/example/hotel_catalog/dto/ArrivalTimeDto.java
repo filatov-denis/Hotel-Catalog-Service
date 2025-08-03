a2c8b2a1e40f5014191f92952109b1cd4ad74efa
Agonny <80535413+Agonny@users.noreply.github.com>
2025-08-03 20:37:10 +0300

    update time format

--- src/main/java/com/example/hotel_catalog/dto/ArrivalTimeDto.java
+++ src/main/java/com/example/hotel_catalog/dto/ArrivalTimeDto.java
@@ -1,5 +1,6 @@
 package com.example.hotel_catalog.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -15,10 +16,12 @@ import java.time.LocalTime;
 @Schema(description = "Модель времени прибытия в отель")
 public class ArrivalTimeDto {
 
+    @JsonFormat(pattern = "HH:mm")
     @NotNull(message = "Время начала прибытия не может быть пустым")
     @Schema(description = "Дата рождения", example = "12:00")
     private LocalTime checkIn;
 
+    @JsonFormat(pattern = "HH:mm")
     @Schema(description = "Время окончания прибытия", example = "14:00")
     private LocalTime checkOut;
 
